b8c4d2d1a192c5b9ce02c6814a525043ec364873
Ashwani <ashwani90125@gmail.com>
2022-07-15 16:48:04 +0530

    Work on fix admin with table change for projects

--- app/Admin/Controllers/ProjectController.php
+++ app/Admin/Controllers/ProjectController.php
@@ -38,13 +38,14 @@ class ProjectController extends AdminController
         $grid->column('scale', __('Scale'));
         $grid->column('opened', __('Opened'));
         $grid->column('scope', __('Scope'));
-        $grid->column('studio', __('Studio'));
+        $grid->column('team', __('Team'));
         $grid->column('image_dim', __('Image dim'));
         $grid->column('next_horizontal', __('Next horizontal'));
         $grid->column('next_empty', __('Next empty'));
         $grid->column('not_show', __('Not show'));
         $grid->column('add_below', __('Add below'));
         $grid->column('is_half', __('Is half'));
+        $grid->column('stage', __('Stage'));
 
         return $grid;
     }
@@ -71,13 +72,14 @@ class ProjectController extends AdminController
         $show->field('scale', __('Scale'));
         $show->field('opened', __('Opened'));
         $show->field('scope', __('Scope'));
-        $show->field('studio', __('Studio'));
+        $show->field('team', __('Team'));
         $show->field('image_dim', __('Image dim'));
         $show->field('next_horizontal', __('Next horizontal'));
         $show->field('next_empty', __('Next empty'));
         $show->field('not_show', __('Not show'));
         $show->field('add_below', __('Add below'));
         $show->field('is_half', __('Is half'));
+        $show->field('stage', __('Stage'));
         $show->projectImages('ProjectImage', function ($images) {
 
             $images->resource('/admin/project-images');
@@ -113,13 +115,14 @@ class ProjectController extends AdminController
         $form->text('scale', __('Scale'));
         $form->text('opened', __('Opened'));
         $form->text('scope', __('Scope'));
-        $form->text('studio', __('Studio'));
+        $form->text('team', __('Team'));
         $form->number('image_dim', __('Image dim'));
         $form->switch('next_horizontal', __('Next horizontal'));
         $form->number('next_empty', __('Next empty'));
         $form->switch('not_show', __('Not show'));
         $form->switch('add_below', __('Add below'));
         $form->number('is_half', __('Is half'));
+        $form->text('stage', __('Stage'));
 
         return $form;
     }
