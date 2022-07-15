9ff48fc7a89462252cf7b5d843854296d75ac08e
Ashwani <ashwani90125@gmail.com>
2022-07-15 11:57:57 +0530

    Fix on about us text

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -10,7 +10,7 @@
                 <div class="row align-items-stretch justify-content-center small-screen">
                     <div class="col-12 position-relative page-title-extra-small text-center d-flex align-items-center justify-content-center flex-column">
                         <h1 class="alt-font text-white opacity-6 margin-20px-bottom">About our company</h1>
-                        <h2 class="text-white alt-font font-weight-500 w-55 md-w-65 sm-w-80 center-col xs-w-100 letter-spacing-minus-1px line-height-50 sm-line-height-45 xs-line-height-30 no-margin-bottom">We are an International architecture practice based on Sydney and Gurugram.</h2>
+                        <h2 class="about-us-main-text text-white alt-font font-weight-500 w-55 md-w-65 sm-w-80 center-col xs-w-100 letter-spacing-minus-1px line-height-50 sm-line-height-45 xs-line-height-30 no-margin-bottom">We are an International architecture practice based on Sydney and Gurugram.</h2>
                     </div>
                     <div class="down-section text-center"><a href="#about" class="section-link"><i class="ti-arrow-down icon-extra-small text-white bg-transparent-black padding-15px-all xs-padding-10px-all border-radius-100"></i></a></div>
                 </div>
