0532f69d233322cbd40f61e7a9d9b3c5cd0b8518
Ashwani <ashwani90125@gmail.com>
2022-09-24 23:41:59 +0530

    Revoke mail access

--- config/mail.php
+++ config/mail.php
@@ -33,14 +33,6 @@ return [
     |
     */
 
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
-
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
